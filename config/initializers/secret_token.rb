31f45af6eea8734cce4ba6d182fbbb5a0c9dd45f
Michael Coughlin <mcoughli@mines.edu>
2013-03-22 13:44:24 -0600

    Commiting initial boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoryApp::Application.config.secret_token = 'd06d23f7c06e06007ba5ab1c8af4a75fa4e42d517fe53a105a1d8505e646ddfbcf3cbe9f5b8d4cee75b0d5f6e9f605e321620f15ec4a599fd746b667fa8abeaf'
