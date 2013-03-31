71c0b31ae69551df8381c06056128f0afe5c0c6b
Michael Coughlin <mcoughli@mines.edu>
2013-03-31 01:33:08 -0600

    refactor edit_path and destroy_path into helper methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+	def edit_path(user)
+		link_to 'Edit', edit_user_path(user)
+	end
+	
+	def delete_path(user)
+		link_to 'Destroy', user, :confirm => 'Are you sure?', :method => :delete
+	end
 end
