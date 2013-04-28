25fe482da563d39bdd86d6de51dad9dfe37a2e71
Michael Coughlin <mcoughli@mines.edu>
2013-04-27 21:26:10 -0600

    added has_attached_file to section model

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,8 @@
 class Section < ActiveRecord::Base
   attr_accessible :chapter_id, :title, :section_text
+  has_attached_file :section_file,
+  	:url => "/assets/files/:id/:style/:basename.:extension",
+  	:path => "cs446/coughlin/#{Rails.env}:url"
 	belongs_to :chapter
 	validates :chapter_id, :presence => true
 	validates :title, :presence => true
