c3a24df73044ec01990b34990e82f4499229486c
Michael Coughlin <mcoughli@mines.edu>
2013-04-27 22:38:00 -0600

    removed validator

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,5 @@
 class Section < ActiveRecord::Base
-  attr_accessible :chapter_id, :title, :section_text, :section_doc, :section_doc_file_name
+  attr_accessible :chapter_id, :title, :section_text, :section_doc, :section_doc_name_file
   has_attached_file :section_doc,
   	:url => "/assets/files/:id/:style/:basename.:extension",
   	:path => "cs446/coughlin/#{Rails.env}:url"
@@ -8,5 +8,5 @@ class Section < ActiveRecord::Base
 	validates :title, :presence => true
 	
 	validates_attachment_presence :section_doc
-	validates_attachment_content_type :attachment, :content_type => [ 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' ]
+	#validates_attachment_content_type :attachment, :content_type => [ 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' ]
 end
